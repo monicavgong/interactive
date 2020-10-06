ce0229b7ae83326c85608d3faac1922010aa3fb9
Monica Gong <monicagong@Monicas-MacBook-Pro.local>
2020-10-06 14:03:06 -0700

    Added ellipses.

--- js/sketch.js
+++ js/sketch.js
@@ -6,40 +6,50 @@ function setup() {
   //creat canvas 500 by 500 px
   createCanvas(windowWidth,windowHeight);
 
-  background(180, 60, 220);
+  background(32, 18, 54);
 
   print(windowWidth);
 }
 
 function draw() {
-  // put drawing code here to run in a loop
 
   //change rectangle x,y coordinates to center
   rectMode(CENTER);
 
   //point is only x,y coordinate
-  strokeWeight(10);
+  strokeWeight(3);
   point(mouseX, mouseY);
 
   //styles for the ellipse
   noStroke();
-  ellipse(300, 500, 150, 150);
+  fill(0, 0, 0);
+  ellipse(850, 450, 600, 600);
 
-  //styles for the rect
-  stroke(40,30,100)
-  strokeWeight(10);
-  fill(50,200,120);
-  rect(800, 300, 200, 300);
+  noStroke();
+  fill(32, 18, 54);
+  ellipse(850, 450, 500, 500);
 
-  //styles for the triangle
-  stroke(40,200,300)
-  strokeWeight(10);
-  noFill();
-  triangle(1200, 50, 1300, 600, 950, 400);
+  noStroke();
+  fill(60, 34, 102);
+  ellipse(850, 450, 400, 400);
+
+  noStroke();
+  fill(82, 46, 140);
+  ellipse(850, 450, 300, 300);
+
+  noStroke();
+  fill(106, 60, 181);
+  ellipse(850, 450, 200, 200);
+
+  noStroke();
+  fill(130, 73, 222);
+  ellipse(850, 450, 100, 100);
+
 
   smooth();
   //styles for the point
   fill(120);
-  stroke(40,200,100)
+  stroke(137, 112, 161)
   print(mouseX);
+
 }
